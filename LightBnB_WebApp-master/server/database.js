9d8216e3164b814430309d73dedc88a8508dc147
Brian LaFond <brian.d.lafond@gmail.com>
2021-10-11 19:03:05 +0000

    update add property function to insert into database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,13 +17,14 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
  const getUserWithEmail = function(email) {
-  return pool.query(`
-  SELECT *
-  FROM users
-  WHERE email = $1
-  `, [email])
-  .then(res => res.rows[0])
-  .catch(err => null);
+  return pool
+    .query(`
+      SELECT *
+      FROM users
+      WHERE email = $1
+      `, [email])
+    .then(res => res.rows[0])
+    .catch(err => null);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -33,13 +34,14 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
  const getUserWithId = function(id) {
-  return pool.query(`
-  SELECT *
-  FROM users
-  WHERE id = $1
-  `, [id])
-  .then(res => res.rows[0])
-  .catch(err => null);
+  return pool
+    .query(`
+      SELECT *
+      FROM users
+      WHERE id = $1
+      `, [id])
+    .then(res => res.rows[0])
+    .catch(err => null);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -50,12 +52,13 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
  const addUser = function(user) {
-  return pool.query(`
-  INSERT INTO users (name, email, password)
-  VALUES($1, $2, $3)
-  RETURNING *
-  `, [user.name, user.email, user.password])
-  .then(res => res.rows[0])
+  return pool
+    .query(`
+      INSERT INTO users (name, email, password)
+      VALUES($1, $2, $3)
+      RETURNING *
+      `, [user.name, user.email, user.password])
+    .then(res => res.rows[0])
 }
 exports.addUser = addUser;
 
@@ -67,17 +70,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
  const getAllReservations = function(guest_id, limit = 10) {
-  return pool.query(`
-  SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
-  FROM reservations
-  JOIN properties ON properties.id = reservations.property_id
-  JOIN property_reviews ON properties.id = property_reviews.property_id
-  WHERE end_date < now()::date
-  AND reservations.guest_id = $1
-  GROUP BY reservations.id, properties.id
-  ORDER BY start_date
-  LIMIT $2;
-  `, [guest_id, limit])
+  return pool
+    .query(`
+      SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+      FROM reservations
+      JOIN properties ON properties.id = reservations.property_id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE end_date < now()::date
+      AND reservations.guest_id = $1
+      GROUP BY reservations.id, properties.id
+      ORDER BY start_date
+      LIMIT $2;
+      `, [guest_id, limit])
   .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
@@ -109,10 +113,31 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+ const addProperty = function(property) {
+  const propertyDetails = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  return pool
+    .query(`
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *
+      `, propertyDetails)
+    .then (res => res.rows[0]);
+};
+
 exports.addProperty = addProperty;
